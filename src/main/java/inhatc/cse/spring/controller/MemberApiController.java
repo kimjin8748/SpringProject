762e0c649512d83b27e55f3ec4b939b98e42a412
김용진 <kimjin8748@gmail.com>
2024-05-21 19:24:48 +0900

    05.21_IoT서버프레임워크 12주차 수업

--- src/main/java/inhatc/cse/spring/controller/MemberApiController.java
+++ src/main/java/inhatc/cse/spring/controller/MemberApiController.java
@@ -5,15 +5,13 @@ import inhatc.cse.spring.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin
 public class MemberApiController {
 
     private final MemberService memberService;
